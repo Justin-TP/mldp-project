00bfac8fea472658691641a73f05e3153d3f1d6d
Justin-TP <2401311B@student.tp.edu.sg>
2026-02-10 21:11:24 +0800

    Changed the link of the saved model

--- airline_flights_streamlit.py
+++ airline_flights_streamlit.py
@@ -8,7 +8,7 @@ import gdown
 
 
 # Google Drive direct download link
-url = f"https://drive.google.com/uc?export=download&id=1BlMv64th03CnAw_0mrSH2AuE3PBvFReF"
+url = f"https://drive.google.com/uc?export=download&id=1A0-UtquOp5NczUNo14zrJZ4UU6N4DqHK"
 
 output = "final_rf_model.pkl"
 gdown.download(url, output, quiet=False)
